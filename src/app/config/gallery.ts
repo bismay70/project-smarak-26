394e3bb392308a8d91c21aaaabb67f604badb1b7
Sujal Kumar Agarwal <sujalnitrkl0596@gmail.com>
2026-02-16 14:31:13 +0530

    gallery data updated

--- src/app/config/gallery.ts
+++ src/app/config/gallery.ts
@@ -1,7 +1,7 @@
 export const galleryImages = [
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
-    "https://res.cloudinary.com/dxi7mifgc/image/upload/v1771152437/N6D3wf_2_djwyjs.svg",
+    "team.webp",
+    "image2.webp",
+    "image3.webp",
+    "image4.webp",
 
 ];
